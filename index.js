33b4050f7ee79e98f6338a7766e3626e00b9fde3
CHAMOK <chamok1995>
2021-09-15 03:02:52 +0600

    santo vai vivo website gift

---
+++ index.js
@@ -0,0 +1,39 @@
+// product even lisitner part 
+const y12=document.getElementById('y12');
+y12.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/phone/vivo-Y12A/';
+})
+const y1s=document.getElementById('y1s');
+y1s.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/phone/vivo-Y1s/';
+})
+const v31=document.getElementById('v31');
+v31.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/phone/vivo-Y31/';
+})
+
+
+const y72=document.getElementById('y72');
+y72.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/phone/vivo-Y72/';
+})
+
+
+
+// card even lisitner part 
+
+
+const card1=document.getElementById('card1');
+card1.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/vivo-smartphones-recommendation/';
+})
+const card2=document.getElementById('card2');
+card2.addEventListener('click',function(){
+    window.location.href='https://www.vivoglobal.ph/how-to-take-perfect-selfie-with-vivo-v21-5G/';
+   
+})
+const card3=document.getElementById('card3');
+card3.addEventListener('click',function(){
+    
+    window.location.href='https://www.vivoglobal.ph/how-to-vlog/';gi
+})
